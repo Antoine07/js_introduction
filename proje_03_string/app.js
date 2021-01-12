19ae671d6ede5b1e128615ace903343fb5c14172
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 12:48:47 +0100

    exo string

--- proje_03_string/app.js
+++ proje_03_string/app.js
@@ -1,7 +1,12 @@
-const container = document.getElementById('root');
+// const permet de définir une valeur constante, vous ne pourrez plus assigner une autre valeur
+// pour cette variable
 
+// la fonction getElementById permet de sélectionner un noeud de l'arbre
+// en fonction d'un identifiant unique #root
+const container = document.getElementById('root');
 console.log(container);
 
+// permet dans container de sélectionner le premier paragraphe p
 const message = container.querySelector('p');
+console.log(message.innerHTML);
 
-console.log(message)
---
+++ proje_03_string/enonce.md
@@ -0,0 +1,8 @@
+# Exercice
+
+lire la chaîne de caractères de la paragraphe, on peut parcourir une chaîne de caractères comme un tableau.
+
+A l'aide d'une boucle récupérez la chaîne de caractères dans le paraphre du div#root puis
+Affichez dans un ul/li l'ensemble des lettres
+Rmq : pensez à créer un ul puis autant de li que lettres. N'oubliez de 
+monter l'ensemble de ces éléments dans le DOM.
