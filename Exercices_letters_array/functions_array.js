ab340611bdbff54d34c55c770da7e87b0d9983de
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 15:52:02 +0100

    exo en plus

--- Exercices_letters_array/functions_array.js
+++ Exercices_letters_array/functions_array.js
@@ -3,3 +3,17 @@
 let message = "Mississippi";
 let chars = [];
 
+for(let i=0; i < message.length; i++){
+    // la méthode push permet d'ajouter des éléments
+    // dans un tableau
+    chars.push(message[i]);
+    // vous pouvez également jouer avec les indices 
+    // des deux tableaux
+    //chars[i] = message[i];
+}
+
+console.log(chars);
+
+// Exercice ajoutez une lettre sur deux dans le tableau suivant
+let m = "anticonstitutionnellement";
+let c = []; // permet d'initialiser un tableau vide
