aeb33705670836ce779db0cbb252609d341b37df
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 16:08:13 +0100

    array letter

--- Exercices_letters_array/functions_array.js
+++ Exercices_letters_array/functions_array.js
@@ -17,3 +17,25 @@ console.log(chars);
 // Exercice ajoutez une lettre sur deux dans le tableau suivant
 let m = "anticonstitutionnellement";
 let c = []; // permet d'initialiser un tableau vide
+
+for(let i = 0; i < m.length; i++){
+    if( i % 2 == 0){
+        c.push(m[i]);
+    }
+}
+console.log(c);
+// on ré-initialise le tableau pour la deuxième solution
+c = [];
+for(let i = 0; i < m.length; i++){
+   if( i % 2 == 1){
+       continue;
+   }
+
+   c.push(m[i]);
+}
+
+console.log(c);
+
+// ré-initialisation du tableau c
+c = [];
+// Troisième solution utilisez deux indices de boucles pour ajouter une lettre sur deux dans le tableau
