28040ca447ab9d427e3a5d4335f8106ff53e44d5
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 10:52:39 +0100

    correction and exo

--- Exercice_if_else/exercice_01.js
+++ Exercice_if_else/exercice_01.js
@@ -5,9 +5,43 @@ Corrigé le code suivant pour traiter toutes les options pour a (test supérieur
 let a = 10;
 
 if(a > 10){
-    console.log('a est strictement supérieur à 10')
+    console.log('a est strictement supérieur à 10');
 }else{
     console.log('a est strictement inférieur à 10');
 }
 
 console.log('a est égal à 10');
+
+if(a > 10){
+    console.log('a est strictement supérieur à 10');
+}
+
+if(a < 10){
+    console.log('a est strictement inférieur à 10');
+}
+
+if( a == 10 ){
+    console.log('a est égale 10');
+}
+
+// Une autre solution avec if/else
+if(a > 10){
+    console.log('a est strictement supérieur à 10');
+}else{
+    if( a == 10 ){
+        console.log('a est égale 10');
+    }else{
+        console.log('a est strictement inférieur à 10');
+    }
+}
+
+// Un autre solution encore if/else if/else
+
+if(a > 10){
+    console.log('a est strictement supérieur à 10');
+}else if(a < 10) {
+    console.log('a est strictement inférieur à 10');
+}else{
+    console.log('a est égale 10');
+}
+
---
+++ Exercice_if_else/exercice_02.js
@@ -0,0 +1,16 @@
+// test dans la condition du if ou else if 
+// Exercice
+
+/*
+Trouvez la valeur de a pour que la condition ci-dessous soit vraie (true)
+Notez que && correspond à AND ou ET en logique 
+
+Rappelons la table de vérité pour le connecteur logique AND 
+
+true AND true = true
+true AND false = false
+false AND true = false
+false AND false = false
+
+*/
+let test = a > 10 && a % 2 == 0;
