a3e40a4a9bc7eafa179cb276142419bd2acbe254
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 11:02:52 +0100

    if/else

--- Exercice_if_else/exercice_02.js
+++ Exercice_if_else/exercice_02.js
@@ -12,5 +12,27 @@ true AND false = false
 false AND true = false
 false AND false = false
 
+le test ci-dessous sera vrai pour toutes les valeurs de a > 0 et pairs
+par exemple a = 12
 */
+let a = 12 ;
 let test = a > 10 && a % 2 == 0;
+
+if(test == true){
+    console.log('le test est vrai');
+}else{
+    console.log('le test est faux');
+}
+
+// Exerice 
+/*
+Trouvez toutes les valeurs de b pour lesquelles le test suivant est vrai
+
+|| OU
+
+true || true = true
+true || false = true
+false || true = true
+false || false = false
+*/
+let test_2 = b > 10 || b % 2 == 0;
