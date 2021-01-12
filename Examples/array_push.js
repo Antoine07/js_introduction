0e364d22a8f6640d24ece58e04d96d822ec94b40
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 14:54:58 +0100

    array

---
+++ Examples/array_push.js
@@ -0,0 +1,18 @@
+
+let numbers = [1,2,3];
+
+// ajoutons un nombre au tableau numbers
+
+numbers.push(7);
+
+console.log(numbers);
+
+// Il existe d'autre fonctions pratiques sur les tableau
+
+console.log(numbers[0]); // lecture
+console.log(numbers); // lecture
+
+// dernier élément de manière dyn
+console.log(numbers[ numbers.length - 1 ]); 
+console.log(numbers); // lecture
+
