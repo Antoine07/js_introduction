d624bf8e2bf54827684592ded7d87e395f4d4954
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 10:30:26 +0100

    if/else

---
+++ Examples/instruction_conditionnelles.js
@@ -0,0 +1,25 @@
+// structure conditionnelle
+let test = true;
+
+if(test == true){
+    console.log('test exact');
+}
+
+// if/else
+if(test == false){
+    console.log('if');
+}else{
+    console.log('else');
+}
+
+// 1. Example
+
+let a = 10;
+
+if(a > 10){
+    console.log('a est strictement supérieur à 10')
+}else{
+    console.log('a est strictement inférieur à 10');
+}
+
+console.log('a est égal à 10');
---
+++ Exercice_if_else/exercice_01.js
@@ -0,0 +1,13 @@
+// Exercice 1
+/*
+Corrigé le code suivant pour traiter toutes les options pour a (test supérieur,inférieur et égale)
+*/
+let a = 10;
+
+if(a > 10){
+    console.log('a est strictement supérieur à 10')
+}else{
+    console.log('a est strictement inférieur à 10');
+}
+
+console.log('a est égal à 10');
