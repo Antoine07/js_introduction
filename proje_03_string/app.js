eff57c4220065d56c191cc452a735bf6be044ee2
Antoine07 <antoine.lucsko@gmail.com>
2021-01-12 14:46:17 +0100

    string

--- proje_03_string/app.js
+++ proje_03_string/app.js
@@ -4,9 +4,24 @@
 // la fonction getElementById permet de sélectionner un noeud de l'arbre
 // en fonction d'un identifiant unique #root
 const container = document.getElementById('root');
-console.log(container);
-
 // permet dans container de sélectionner le premier paragraphe p
-const message = container.querySelector('p');
-console.log(message.innerHTML);
+// innerHTML permet de récupérer le texte dans le paragraphe
+console.log(container.querySelector('p'));
+console.log(container.querySelector('p').innerHTML);
 
+const message = container.querySelector('p').innerHTML;
+
+// création d'un ul
+const ul = document.createElement('ul');
+
+// on parcourt le message
+for(let i= 0; i < message.length; i++){
+    // création des li
+    let li = document.createElement('li');
+    li.innerHTML = message[i];
+    // on ajoute les li dans le ul
+    ul.appendChild(li);
+}
+
+// puis on ajoute le ul avec les li dans le ul
+container.appendChild(ul);
