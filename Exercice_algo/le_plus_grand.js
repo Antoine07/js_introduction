2fcc151f8d5641cc80623d85be37e4a2e393fb00
Antoine07 <antoine.lucsko@gmail.com>
2021-01-13 14:50:48 +0100

    algo

--- Exercice_algo/le_plus_grand.js
+++ Exercice_algo/le_plus_grand.js
@@ -6,3 +6,10 @@ grand nombre du tableau numbers ci-dessous :
 let numbers = [11, 2, 4, 67, 19, 10, 5, 17, 0, 7];
 
 let candidate = numbers[0];
+
+for(let i =1 ;i < numbers.length; i++){
+    if(numbers[i] > candidate) {
+        candidate = numbers[i];
+    }
+}
+console.log('Cdt plus grand :', candidate);
---
+++ Exercice_algo/max_3_nombres.js
@@ -0,0 +1,11 @@
+/*
+Exercice 
+Ecrire un script permettant de déterminer le nombre le plus grand parmi les 3 valeurs
+a, b et c ci-dessous.
+*/
+
+// let a = 11;
+// let b = 17;
+// let c = 10;
+
+let a = 11, b = 17, c = 10;
