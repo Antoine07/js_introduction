603ee00b84619547c3467a49dd5a3db11354c190
Antoine07 <antoine.lucsko@gmail.com>
2021-01-13 10:41:53 +0100

    reverse tableau

---
+++ Exercices_letters_array/reverse.js
@@ -0,0 +1,12 @@
+/*
+ Exercice soit la chaîne de caractères message suivante, créez un tableau pour récupérer
+ chacune des lettres. Puis inverser l'ordre des lettres dans le tableau.
+
+ rmq : vous n'utiliserez pas de fonction JS déjà définie.
+*/
+
+let message = "Mississippi";
+let letters = [];
+
+// reverse tableau
+let reverses_letters = [];
