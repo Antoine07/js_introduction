dcdf710c6e06d495e29e6267b4a9b6e7f472c0e7
Antoine07 <antoine.lucsko@gmail.com>
2021-01-13 14:16:05 +0100

    algo le plus grand

---
+++ Exercice_algo/le_plus_grand.js
@@ -0,0 +1,8 @@
+/*
+Exercice de recherche, à l'aide d'un algorithme écrit en JS trouvez le plus 
+grand nombre du tableau numbers ci-dessous :
+*/
+
+let numbers = [11, 2, 4, 67, 19, 10, 5, 17, 0, 7];
+
+let candidate = numbers[0];
