bda0275f510e5c928731491f21e84c5d1cf0007a
Antoine07 <antoine.lucsko@gmail.com>
2021-01-14 17:03:19 +0100

    litterauxé

--- Exercice_litteraux/populations.js
+++ Exercice_litteraux/populations.js
@@ -39,9 +39,104 @@ let nbStudentA = 0;
 for(let i=0; i < populations.length; i++){
     let student = populations[i] ; 
     // includes peut s'utiliser sur une chaîne de caractères
+    // JS est sensible à la casse par exemple a est différent de A
     if( student.name.includes('a') || student.name.includes('A') ) {
         nbStudentA++;
     }
 }
 
 console.log("Nombre d'étudiants ayant au moins un a dans son nom :", nbStudentA);
+
+/*
+
+
+1. Créez une fonction search elle admet un paramètre name. 
+Cette fonction recherchera un nom dans le littéral populations. 
+Elle retournera true ou false selon qu'elle trouvera ou nom le nom rechercher. 
+Cette première version est sensible à la casse.
+
+*/
+
+function search(name){
+
+    for(let i = 0; i < populations.length; i++){
+        let student = populations[i];
+
+        // c'est plus sûr de mettre includes pour les pb d'espaces avant ou après
+
+        // sinon une autre solution, en utilisant trim qui supprime les espaces avant et après la chaîne :
+        // student.name.trim() == name.trim()
+        if(student.name.includes(name)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// console.log(search("Brice"));
+
+/*
+2. Faites une deuxième fonction qu'on appelera search_casse, elle sera insensible à la casse.
+*/
+
+function search_casse(name){
+    name = name.toLowerCase(); // mettre en minuscule la variable du paramètre de la fonction
+
+    for(let i = 0; i < populations.length; i++){
+        let student = populations[i];
+        let keyName = student.name.toLowerCase(); // on met également le name du littéral en minuscule
+        if(keyName.includes(name)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log("search_casse")
+console.log(search_casse("BRICE"));
+console.log(search_casse("brice"));
+console.log(search_casse("Brice"));
+console.log(search_casse("BriCe"));
+
+console.log(search_casse(" BriCe "));
+
+/*
+3. Ajoutez une clé age aux littéraux dans le tableau populations et donnez les ages respectifs suivants aux personnes :
+
+*/
+
+// On peut ajouter une clé aux littéraux ceci les modifiera
+// for(let i =0; i < populations.length; i++){
+//     populations[i].age = 18;
+// }
+
+// console.log(populations);
+
+const ages = { 
+    "Alan" : 16,
+    "Albert" : 19,
+    "Jhon" : 16,
+    "Brice": 90,
+    "Alexendra" : 77,
+    "Brad" : 89,
+    "Carl" : 67,
+    "Dallas" : 90,
+    "Dennis" : 75,
+    "Edgar" : 23,
+    "Erika" : 30,
+    "Isaac" : 56,
+    "Ian" : 45
+}
+
+for(let i =0; i < populations.length; i++){
+    // le nom c'est la clé du littéral ages
+    let name = populations[i].name ;
+    // console.log(ages[name]);
+    // on crée une nouvelle clé dans chaque littéral
+    // puis on assigne pour cette clé l'age correspondant
+    populations[i].age = ages[name];
+}
+
+console.log(populations);
